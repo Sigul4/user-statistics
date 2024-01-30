f378ba0a597e70fb5c8c35f22db8d371441d4bbc
Roman Tsyhulov <roman.tsyhulov@gmail.com>
2024-01-31 00:58:16 +0200

    applied dashboard page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { DashboardService } from './services/dashboard.service';
+import { ReportData } from '../shared/interfaces/report-data.interface';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  public reports: ReportData[] = [];
+  private unsubscribe$: Subject<void> = new Subject<void>();
+  constructor(private dashboardService: DashboardService) {}
 
+  ngOnInit(): void {
+    this.loadReports();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  private loadReports(): void {
+    this.dashboardService
+      .getReports()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(
+        (reports: ReportData[]) => {
+          this.reports = reports;
+        },
+        (error) => {
+          console.error('Error loading reports:', error);
+        }
+      );
+  }
 }
